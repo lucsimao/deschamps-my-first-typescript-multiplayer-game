01648ea09b1f4959b4768883fbf66f6be76b818d
lucsimao <lucasimao64@gmail.com>
2021-06-24 21:57:03 -0300

    feat: creating player
     - creating IPlayer interface
     - adding IPlayer to IState interface
     - creating add and remove player in Game class
     - creating IGameElement interface

--- src/models/interfaces/IState.ts
+++ src/models/interfaces/IState.ts
@@ -1,5 +1,7 @@
+import { IPlayer } from './states-attributes/IPlayer';
 import { IScreen } from './states-attributes/IScreen';
 
 export interface IState {
+  players: { [playerId: string]: IPlayer };
   screen: IScreen;
 }
