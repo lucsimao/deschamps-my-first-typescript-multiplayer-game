c798cfb1cf234c8713c6227d3be37c90c38975c0
lucsimao <lucasimao64@gmail.com>
2021-06-27 15:37:04 -0300

    feat: adding tests for 57f3896

--- src/__tests__/game.test.ts
+++ src/__tests__/game.test.ts
@@ -1,3 +1,4 @@
+import { Move } from '@src/models/enum/Move';
 import { Game } from '@src/models/Game';
 import { ICommand } from '@src/models/interfaces/observers/ICommand';
 
@@ -21,21 +22,89 @@ describe('Game tests', () => {
       expect(game.state).toEqual(state);
     });
 
-    it('should add a player when invoke game.addPlayer', () => {
-      const player = { playerId: 'player1', x: 10, y: 10 };
+    describe('Player tests', () => {
+      it('should add a player when invoke game.addPlayer', () => {
+        const player = { playerId: 'player1', x: 10, y: 10 };
 
-      game.addPlayer(player);
-      expect(game.state.players['player1']).toEqual(player);
-    });
+        game.addPlayer(player);
+        expect(game.state.players['player1']).toEqual(player);
+      });
 
-    it('should remove a player when invoke player.removePlayer', () => {
-      const player = { playerId: 'player1', x: 10, y: 10 };
+      it('should remove a player when invoke player.removePlayer', () => {
+        const player = { playerId: 'player1', x: 10, y: 10 };
 
-      game.addPlayer(player);
-      expect(game.state.players['player1']).toEqual(player);
+        game.addPlayer(player);
+        expect(game.state.players['player1']).toEqual(player);
 
-      game.removePlayer(player);
-      expect(game.state.players['player1']).toBeUndefined();
+        game.removePlayer(player);
+        expect(game.state.players['player1']).toBeUndefined();
+      });
+
+      describe('Move Player tests', () => {
+        it('should move player up when invoke move player Up', () => {
+          const player = { playerId: 'player1', x: 1, y: 5 };
+          const command = { move: Move.UP, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].y).toEqual(4);
+        });
+
+        it('should move player down when invoke move player Down', () => {
+          const player = { playerId: 'player1', x: 1, y: 5 };
+          const command = { move: Move.DOWN, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].y).toEqual(6);
+        });
+
+        it('should move player left when invoke move player Left', () => {
+          const player = { playerId: 'player1', x: 1, y: 5 };
+          const command = { move: Move.LEFT, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].x).toEqual(0);
+        });
+
+        it('should move player right when invoke move player Right', () => {
+          const player = { playerId: 'player1', x: 1, y: 5 };
+          const command = { move: Move.RIGHT, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].x).toEqual(2);
+        });
+
+        it('should not move player up when player is in top of the screen', () => {
+          const player = { playerId: 'player1', x: 1, y: 0 };
+          const command = { move: Move.UP, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].y).toEqual(0);
+        });
+
+        it('should not move player down when player is in bottom of the screen', () => {
+          const player = { playerId: 'player1', x: 1, y: 19 };
+          const command = { move: Move.DOWN, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].y).toEqual(19);
+        });
+
+        it('should not move player left when player is in most left of the screen', () => {
+          const player = { playerId: 'player1', x: 0, y: 10 };
+          const command = { move: Move.LEFT, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].x).toEqual(0);
+        });
+
+        it('should not move player right when player is in most right of the screen', () => {
+          const player = { playerId: 'player1', x: 19, y: 10 };
+          const command = { move: Move.RIGHT, player };
+          game.addPlayer(player);
+          game.movePlayer(command);
+          expect(game.state.players[player.playerId].x).toEqual(19);
+        });
+      });
     });
 
     it('should add a fruit when invoke game.addFruit', () => {
