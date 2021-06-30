d636cdd26621d6264d312f1d5e95c6dfdd94f483
lucsimao <lucasimao64@gmail.com>
2021-06-30 18:59:59 -0300

    refactor: adjusting Move command  - adjusting enum/Move.ts  - adjusting observer/IMoveCommand.ts

--- src/models/interfaces/observers/IMoveCommand.ts
+++ src/models/interfaces/observers/IMoveCommand.ts
@@ -1,7 +1,6 @@
 import { Move } from '@src/models/enum/Move';
-import { IPlayer } from '../states-attributes/IPlayer';
 
 export interface IMoveCommand {
   move: Move;
-  player: IPlayer;
+  playerId: string;
 }
